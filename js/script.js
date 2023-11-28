434876e6038101c095df649400a78c0741c84903
diego <diego.scavalcanti@gmail.com>
2023-11-28 17:58:52 -0300

    fifteen commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 const menuButton = document.querySelector("#menu-button")
-
 const menuElement = document.querySelector("#menu")
 
 // const closeButton = document.querySelector(".close-button");
@@ -25,6 +24,20 @@ window.onload = function() {
    document.getElementById('exibicaoNome').innerHTML = nome;
 }
 
+const xzao =document.querySelector('#xzao')
+
 xzao.addEventListener("click", function () {
    menuElement.classList.remove("open");
  });
+
+function alertar() {
+   if(document.getElementById("tipo").value == "medico") {
+      document.getElementById("medico").style.visibility = "visible";
+      document.getElementById("paciente").style.visibility = "hidden";
+   } else {
+      document.getElementById("paciente").style.visibility = "visible";
+      document.getElementById("medico").style.visibility = "hidden";
+   }
+}
+   
+ 
